4360443906114dd97c801a3bbb07d1f6c00d8808
Mani Nesan <mani@Manis-MacBook-Pro.local>
2024-10-22 14:42:38 +0800

    Initial commit

---
+++ tests/unit/test_basecaller_on_aws_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from basecaller_on_aws.basecaller_on_aws_stack import BasecallerOnAwsStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in basecaller_on_aws/basecaller_on_aws_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = BasecallerOnAwsStack(app, "basecaller-on-aws")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
